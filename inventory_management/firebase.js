0f9c8135ffe73ae2a43c262575605d2723d7e549
ryanhoang <rhoang23@uw.edu>
2024-08-08 01:11:50 -0700

    Initial commit

--- inventory_management/firebase.js
+++ inventory_management/firebase.js
@@ -7,13 +7,12 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhdkJaZPucnFgXsbCBP8vji2RgGlKZGms",
-  authDomain: "inventory-management-1e4fb.firebaseapp.com",
-  projectId: "inventory-management-1e4fb",
-  storageBucket: "inventory-management-1e4fb.appspot.com",
-  messagingSenderId: "615856920090",
-  appId: "1:615856920090:web:a5529448eef16b2656e3a3",
-  measurementId: "G-LMRNDHQ1L9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
